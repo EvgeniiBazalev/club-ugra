e195b7b56105cd01be0f22c518387bdc5f886157
EvgeniiBazalev <bazalevj@gmail.com>
2023-12-17 21:21:12 +0500

    main page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SidebarLayouts from "./components/UI/SidebarLayouts/SidebarLayouts";
+import FooterMain from "./components/UI/Footer/FooterMain";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <SidebarLayouts>{children}</SidebarLayouts>
+        <FooterMain />
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import CarouselMain from "./components/UI/CarouselMain/CarouselMain";
+import Pricing from "./components/UI/Pricing/Pricing";
+import Feedbacks from "./components/UI/Feedbacks/Feedbacks";
 
 export default function Home() {
   return (
     <main>
       <CarouselMain />
+      <Pricing />
+      <Feedbacks />
     </main>
   );
 }
