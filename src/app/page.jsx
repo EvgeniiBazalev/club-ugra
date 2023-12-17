9f73a3178a8ce67da60ab009d096b8659504b91e
EvgeniiBazalev <bazalevj@gmail.com>
2023-12-17 20:41:44 +0500

    base of page

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import CarouselMain from "./components/UI/CarouselMain/CarouselMain";
+
+export default function Home() {
+  return (
+    <main>
+      <CarouselMain />
+    </main>
+  );
+}
