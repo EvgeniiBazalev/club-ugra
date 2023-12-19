6c86b53c18a53bf82410f678f69c3c320fd0b90e
EvgeniiBazalev <bazalevj@gmail.com>
2023-12-19 20:59:21 +0500

    footer

--- src/app/components/UI/Footer/FooterMain.jsx
+++ src/app/components/UI/Footer/FooterMain.jsx
@@ -5,41 +5,31 @@ const FooterMain = () => {
     <Footer>
       <div className="mx-auto flex max-w-screen-xl flex-col items-center p-4 text-center md:p-8 lg:p-10 [&>div]:w-fit">
         <Footer.Brand
-          alt="Flowbite logo"
-          href="https://flowbite.com"
-          name="Flowbite"
-          src="https://flowbite.com/docs/images/logo.svg"
+          alt="Logo"
+          href="#"
+          name="Региональная общественная организация ХМАО-Югры КЛУБ ЕДИНОБОРСТВ ЮГРА"
+          src="main/logo3.ico"
         />
         <p className="my-6 text-gray-500 dark:text-gray-400">
-          Open-source library of over 400+ web components and interactive
-          elements built for better web.
+          Среди трудностей и вызовов, мы — единое целое, Клуб Югра берется за
+          каждую возможность, воплощая силу духа в движениях борьбы, стремясь к
+          великим достижениям!
         </p>
         <Footer.LinkGroup className="mb-6 flex flex-wrap items-center justify-center text-gray-900 dark:text-white text-base">
           <Footer.Link href="#" className="mr-4 hover:underline md:mr-6 ">
-            About
+            О клубе
           </Footer.Link>
           <Footer.Link href="#" className="mr-4 hover:underline md:mr-6">
-            Premium
+            Новости
           </Footer.Link>
-          <Footer.Link href="#" className="mr-4 hover:underline md:mr-6 ">
-            Campaigns
-          </Footer.Link>
+
           <Footer.Link href="#" className="mr-4 hover:underline md:mr-6">
-            Blog
-          </Footer.Link>
-          <Footer.Link href="#" className="mr-4 hover:underline md:mr-6">
-            Affiliate Program
-          </Footer.Link>
-          <Footer.Link href="#" className="mr-4 hover:underline md:mr-6">
-            FAQs
-          </Footer.Link>
-          <Footer.Link href="#" className="mr-4 hover:underline md:mr-6">
-            Contact
+            Контакты
           </Footer.Link>
         </Footer.LinkGroup>
         <Footer.Copyright
-          by="Flowbite™. All Rights Reserved."
-          href="https://flowbite.com"
+          by="Клуб Югра™. Все права защищены."
+          href="#"
           year={2023}
         />
       </div>
