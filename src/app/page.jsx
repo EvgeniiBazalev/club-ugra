627f0995d812ec0a7d137eece2d55e4058053802
EvgeniiBazalev <bazalevj@gmail.com>
2023-12-20 17:45:27 +0500

    refactoring project

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import CarouselMain from "@/components/UI/CarouselMain/CarouselMain";
-import Pricing from "@/components/UI/Pricing/Pricing";
-import Feedbacks from "@/components/UI/Feedbacks/Feedbacks";
-import FAQ from "@/components/UI/FAQ/FAQ";
-import FAQGrid from "@/components/UI/FAQ/FAQGrid";
+import CarouselMain from "@/components/PagesComponents/MainPage/CarouselMain/CarouselMain";
+import Pricing from "@/components/PagesComponents/MainPage/Pricing/Pricing";
+import Feedbacks from "@/components/PagesComponents/MainPage/Feedbacks/Feedbacks";
+import FAQ from "@/components/PagesComponents/MainPage/FAQ/FAQ";
+import FAQGrid from "@/components/PagesComponents/MainPage/FAQ/FAQGrid";
 
 export default function Home() {
   return (
