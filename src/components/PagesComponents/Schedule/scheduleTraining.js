6704f0cff324c019d152e400777a9dc3d552ed0c
EvgeniiBazalev <bazalevj@gmail.com>
2023-12-25 08:57:02 +0500

    schedule js

---
+++ src/components/PagesComponents/Schedule/scheduleTraining.js
@@ -0,0 +1,228 @@
+const scheduleTraining = {
+  1: [
+    {
+      key: 1.1,
+      time: "9:00 - 10:30",
+      title: "Тайский бокс - спорстмены",
+      trainerId: 3,
+    },
+    {
+      key: 1.2,
+      time: "10:00 - 11:30",
+      title: "Тайский бокс",
+      trainerId: 4,
+    },
+    {
+      key: 1.3,
+      time: "15:00 - 16:30",
+      title: "Тайский бокс",
+      trainerId: 5,
+    },
+    {
+      key: 1.4,
+      time: "16:30 - 18:00",
+      title: "Тайский бокс",
+      trainerId: 6,
+    },
+    {
+      key: 1.5,
+      time: "18:00 - 19:30",
+      title: "Кикбоксинг",
+      trainerId: 7,
+    },
+  ],
+  2: [
+    {
+      key: 1.1,
+      time: "9:00 - 10:30",
+      title: "Тайский бокс - спорстмены",
+      trainerId: 3,
+    },
+    {
+      key: 1.2,
+      time: "10:00 - 11:30",
+      title: "Тайский бокс",
+      trainerId: 4,
+    },
+    {
+      key: 1.3,
+      time: "15:00 - 16:30",
+      title: "Тайский бокс",
+      trainerId: 5,
+    },
+    {
+      key: 1.4,
+      time: "16:30 - 18:00",
+      title: "Тайский бокс",
+      trainerId: 6,
+    },
+    {
+      key: 1.5,
+      time: "18:00 - 19:30",
+      title: "Кикбоксинг",
+      trainerId: 7,
+    },
+  ],
+  3: [
+    {
+      key: 1.1,
+      time: "9:00 - 10:30",
+      title: "Тайский бокс - спорстмены",
+      trainerId: 3,
+    },
+    {
+      key: 1.2,
+      time: "10:00 - 11:30",
+      title: "Тайский бокс",
+      trainerId: 4,
+    },
+    {
+      key: 1.3,
+      time: "15:00 - 16:30",
+      title: "Тайский бокс",
+      trainerId: 5,
+    },
+    {
+      key: 1.4,
+      time: "16:30 - 18:00",
+      title: "Тайский бокс",
+      trainerId: 6,
+    },
+    {
+      key: 1.5,
+      time: "18:00 - 19:30",
+      title: "Кикбоксинг",
+      trainerId: 7,
+    },
+  ],
+  4: [
+    {
+      key: 1.1,
+      time: "9:00 - 10:30",
+      title: "Тайский бокс - спорстмены",
+      trainerId: 3,
+    },
+    {
+      key: 1.2,
+      time: "10:00 - 11:30",
+      title: "Тайский бокс",
+      trainerId: 4,
+    },
+    {
+      key: 1.3,
+      time: "15:00 - 16:30",
+      title: "Тайский бокс",
+      trainerId: 5,
+    },
+    {
+      key: 1.4,
+      time: "16:30 - 18:00",
+      title: "Тайский бокс",
+      trainerId: 6,
+    },
+    {
+      key: 1.5,
+      time: "18:00 - 19:30",
+      title: "Кикбоксинг",
+      trainerId: 7,
+    },
+  ],
+  5: [
+    {
+      key: 1.1,
+      time: "9:00 - 10:30",
+      title: "Тайский бокс - спорстмены",
+      trainerId: 3,
+    },
+    {
+      key: 1.2,
+      time: "10:00 - 11:30",
+      title: "Тайский бокс",
+      trainerId: 4,
+    },
+    {
+      key: 1.3,
+      time: "15:00 - 16:30",
+      title: "Тайский бокс",
+      trainerId: 5,
+    },
+    {
+      key: 1.4,
+      time: "16:30 - 18:00",
+      title: "Тайский бокс",
+      trainerId: 6,
+    },
+    {
+      key: 1.5,
+      time: "18:00 - 19:30",
+      title: "Кикбоксинг",
+      trainerId: 7,
+    },
+  ],
+  6: [
+    {
+      key: 1.1,
+      time: "9:00 - 10:30",
+      title: "Тайский бокс - спорстмены",
+      trainerId: 3,
+    },
+    {
+      key: 1.2,
+      time: "10:00 - 11:30",
+      title: "Тайский бокс",
+      trainerId: 4,
+    },
+    {
+      key: 1.3,
+      time: "15:00 - 16:30",
+      title: "Тайский бокс",
+      trainerId: 5,
+    },
+    {
+      key: 1.4,
+      time: "16:30 - 18:00",
+      title: "Тайский бокс",
+      trainerId: 6,
+    },
+    {
+      key: 1.5,
+      time: "18:00 - 19:30",
+      title: "Кикбоксинг",
+      trainerId: 7,
+    },
+  ],
+  7: [
+    {
+      key: 1.1,
+      time: "9:00 - 10:30",
+      title: "Тайский бокс - спорстмены",
+      trainerId: 3,
+    },
+    {
+      key: 1.2,
+      time: "10:00 - 11:30",
+      title: "Тайский бокс",
+      trainerId: 4,
+    },
+    {
+      key: 1.3,
+      time: "15:00 - 16:30",
+      title: "Тайский бокс",
+      trainerId: 5,
+    },
+    {
+      key: 1.4,
+      time: "16:30 - 18:00",
+      title: "Тайский бокс",
+      trainerId: 6,
+    },
+    {
+      key: 1.5,
+      time: "18:00 - 19:30",
+      title: "Кикбоксинг",
+      trainerId: 7,
+    },
+  ],
+};
+
+export default scheduleTraining;
