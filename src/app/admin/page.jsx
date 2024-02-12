83e1fcbdd1ab14469820726a8dfa96dbdccdc386
EvgeniiBazalev <bazalevj@gmail.com>
2024-02-12 15:14:10 +0500

    dashboard fish

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,8 +1,14 @@
+import Dashboard from "@/components/AdminComponents/Dashboard/Dashboard";
 import SignOut from "@/components/Auth/SignOut";
 import React from "react";
 
 const page = () => {
-  return <SignOut />;
+  return (
+    <>
+      <Dashboard />
+      <SignOut />
+    </>
+  );
 };
 
 export default page;
