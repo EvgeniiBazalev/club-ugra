4b64e92654b9e4e2e81294e075c01e1e51e7f592
EvgeniiBazalev <bazalevj@gmail.com>
2024-02-12 12:12:55 +0500

    remove editing

--- src/components/AdminComponents/Students/StudentsList.jsx
+++ src/components/AdminComponents/Students/StudentsList.jsx
@@ -87,14 +87,14 @@ const StudentsList = (props) => {
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                       {person.streetAddress}
                     </td>
-                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                    {/* <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                       <a
                         href="#"
                         className="text-indigo-600 hover:text-indigo-900"
                       >
                         Edit<span className="sr-only">, {person.lastName}</span>
                       </a>
-                    </td>
+                    </td> */}
                     <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                       <a
                         href="#"
