7f76b65ad673b9b73bedd00cc5298d5c972128e4
EvgeniiBazalev <bazalevj@gmail.com>
2024-02-12 15:39:44 +0500

    router for admin panel

--- src/components/Auth/SignOut.jsx
+++ src/components/Auth/SignOut.jsx
@@ -5,20 +5,41 @@ import { Button } from "flowbite-react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function SignOut() {
   const [user, setUser] = useState();
 
   const supabase = createClientComponentClient();
+  const router = useRouter();
 
   useEffect(() => {
     const getData = async () => {
-      const { data, error } = await supabase.auth.getUser();
-      setUser(data.user);
+      try {
+        const { data, error } = await supabase.auth.getUser();
+
+        if (data) {
+          setUser(data.user);
+        }
+
+        if (error) {
+          console.error(
+            "Произошла ошибка при получении данных пользователя:",
+            error
+          );
+          router.push("/");
+          return; // Остановить выполнение, если возникла ошибка
+        }
+      } catch (error) {
+        console.error(
+          "Произошла ошибка при получении данных пользователя:",
+          error
+        );
+      }
     };
 
-    getData();
-  }, [supabase.auth]);
+    getData(); // Вызов функции получения данных
+  }, [supabase.auth, router]);
 
   const handleSignOut = async () => {
     await supabase.auth.signOut();
